8312a35ef60b12b9bad689a1ee63da014451ed16
James Kaupert <jameskaupert@gmail.com>
2017-12-26 07:35:39 -0500

    add markdown parsing

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,20 +1,15 @@
 import React from "react";
 
-export default class Editor extends React.Component {
-  state = {
-    editorText: 'puppies are da best!'
-  }
-  render() {
-    return (
-      <div className="editor-container">
-        <h4 className="editor__title">Type it in Markdown</h4>
-        <textarea
-          id="editor"
-          className="editor"
-        >
-          {this.state.editorText}
-        </textarea>
-      </div>
-    );
-  }
-}
+const Editor = props => (
+  <div className="editor-container">
+    <h4 className="editor__title">Type it in Markdown</h4>
+    <textarea
+      id="editor"
+      className="editor"
+      value={props.value}
+      onChange={props.handleEditText}
+    />
+  </div>
+);
+
+export { Editor as default };
--- src/components/MarkdownPreviewerApp.js
+++ src/components/MarkdownPreviewerApp.js
@@ -1,14 +1,62 @@
-import React from 'react';
-import Editor from './Editor'
-import Previewer from './Previewer'
+import React from "react";
+import Editor from "./Editor";
+import Previewer from "./Previewer";
+
+const initialText = `
+# Header 1 Preview
+
+## Header 2 Preview
+
+[Link Preview](https://www.google.com)
+
+Some inline code \`hello world\` for your enjoyment
+
+A code block
+  \`\`\`
+  const helloMarkdown = () => {
+      return 'Hello, Markdown'
+  }
+  \`\`\`
+
+Some list items:
+- Item 1
+- Item 2
+- Item 3
+
+Block Quote
+> This is
+> a blockquote
+> for your enjoyment
+
+Some **bold** and *italicized* text
+
+Images:
+![dachschund](http://images2.fanpop.com/image/photos/13600000/-Dachshunds-dachshunds-13634827-1680-1050.jpg)
+`
 
 export default class MarkdownPreviewerApp extends React.Component {
-    render() {
-        return (
-            <div className='markdown-previewer-app'>
-              <Editor />
-              <Previewer />
-            </div>
-        )
-    }
+  state = {
+    value: initialText
+  };
+
+  handleEditText = e => {
+    const value = e.target.value;
+    this.setState(() => ({ value }));
+  };
+  componentDidMount() {
+      console.log(this.state.value);
+  }
+  render() {
+    return (
+      <div className="markdown-previewer-app">
+      <Editor
+        value={this.state.value}
+        handleEditText={this.handleEditText}
+      />
+      <Previewer
+        editorText={this.state.value}
+      />
+      </div>
+    );
+  }
 }
