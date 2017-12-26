39c9979ab480e2245b88cc1fc6b38009ee917f2f
James Kaupert <jameskaupert@gmail.com>
2017-12-26 05:40:56 -0500

    set up initial state for editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-const Editor = props => (
-  <div className="editor-container">
-    <h4 className="editor__title">Type it in Markdown</h4>
-    <textarea id="editor" className="editor"></textarea>
-  </div>
-);
-
-export { Editor as default };
+export default class Editor extends React.Component {
+  state = {
+    editorText: 'puppies are da best!'
+  }
+  render() {
+    return (
+      <div className="editor-container">
+        <h4 className="editor__title">Type it in Markdown</h4>
+        <textarea
+          id="editor"
+          className="editor"
+        >
+          {this.state.editorText}
+        </textarea>
+      </div>
+    );
+  }
+}
